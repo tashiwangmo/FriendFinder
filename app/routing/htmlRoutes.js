7c24a408441d2e1cce3a161e27f6cb6319aa2311
tashiwangmo <tsultrimlhewa@tsultrims-air.ad.ascendlearning.com>
2018-08-06 18:03:25 -0500

    final1

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+// A GET Route to /survey which should display the survey page.
+// A default, catch-all route that leads to home.html which displays the home page.
+// Pull in required dependencies
+
+var path = require('path');
+
+// Export HTML routes
+module.exports = function(app) {
+	// console.log('___ENTER htmlRoutes.js___');
+
+	// Home page
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+
+	// Survey page
+	app.get('/survey', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
+	});
+};
---
+++ server.js
@@ -0,0 +1,5 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
