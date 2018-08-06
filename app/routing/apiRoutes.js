2c5b388b3fc75d59015c07eb54bde3a34479aa81
tashiwangmo <tsultrimlhewa@er-10-129-156-52.wireless.umn.edu>
2018-08-06 18:57:14 -0500

    final2

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,4 @@
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-// Pull in required dependencies
+// Dependencies
 var path = require('path');
 
 // Import the list of friend entries
@@ -8,7 +6,6 @@ var friends = require('../data/friends.js');
 
 // Export API routes
 module.exports = function(app) {
-	// console.log('___ENTER apiRoutes.js___');
 
 	// Total list of friend entries
 	app.get('/api/friends', function(req, res) {
